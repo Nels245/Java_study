74584e42f6d2aaae905b16ff7d52d16b34c7e298
Nels245 <boldirevpavel97@yandex.ru>
2023-03-01 22:06:51 +0300

    Add DZ_01

---
+++ Homework/DZ_01.java
@@ -0,0 +1,11 @@
+// Задание 1. Написать программу вычисления n-ого треугольного числа.
+
+public class DZ_01 {
+    public static void main(String[] args) {
+        int n = in
+        int[] arr = new int[];
+        for (i = 1, ) {
+            
+        }
+    }
+}
